59e81f2d1b76e93b3405869af13d2d3cc68622fa
TheGuy191919 <evan_1998@sina.com>
2014-07-26 15:05:06 -0400

    Connects to server

--- src/Team5163/TeamTrackerClient.java
+++ src/Team5163/TeamTrackerClient.java
@@ -4,6 +4,10 @@
  */
 package Team5163;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author Yiwen Dong
@@ -13,7 +17,8 @@ public class TeamTrackerClient {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        Socket socket = new Socket("localhost", 5163);
+        
     }
 }
