24c9596b44a2913ee9c6bcc2f4863f04c3e47dc9
TheGuy191919 <evan_1998@sina.com>
2014-07-26 20:41:46 -0400

    Curently sending test messages

--- src/Team5163/TeamTrackerClient.java
+++ src/Team5163/TeamTrackerClient.java
@@ -5,6 +5,7 @@
 package Team5163;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -13,12 +14,15 @@ import java.net.UnknownHostException;
  * @author Yiwen Dong
  */
 public class TeamTrackerClient {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws UnknownHostException, IOException {
         Socket socket = new Socket("localhost", 5163);
-        
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        String message = "01Hi, This is a test message sent by a client1234567890";
+        out.println(message);
+        socket.close();
     }
 }
